db00f0b7eda467dd922b814db389be2a952ab5cd
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-09-24 12:15:01 +0530

    data types

---
+++ Basic/basic1.js
@@ -0,0 +1,36 @@
+// console.log("hello world!");
+// log -- function 
+
+// console.warn("this is warning function..")
+
+// console.error("this is error function..")
+
+
+// datatypes : number , string , boolean , undefined
+
+a = 10;
+
+console.log("value of a = ",a);
+console.log("type of a = ",typeof(a))
+
+b = 12.5;
+console.log("value of b = ",b);
+console.log("type of b = ",typeof(b));
+
+c = "ram";
+console.log("value of c = ",c);
+console.log("type of c = ",typeof(c));
+
+isActive = true;
+console.log("active = ",isActive);
+console.log("active = ",typeof(isActive));
+
+var x;
+console.log("value of x = ",x);
+console.log("value of x = ",typeof(x));
+
+
+
+
+
+
