6197e6d9bc07acf8c7d50883e2d1cd53d704c25f
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-10-08 12:34:15 +0530

    airthmatic

---
+++ Operator/Airthmatic.js
@@ -0,0 +1,28 @@
+var no1 = 15;
+var no2 = 13;
+
+
+
+let add = no1 + no2;
+console.log("Addition = ",add);
+
+let sub = no1 - no2;
+console.log("Subraction = ",sub);
+
+let mul = no1 * no2;
+console.log("Multiplication = ",mul);
+
+let div = no1 / no2;    
+console.log("Division = ",div);
+
+let mod = no1 % no2;
+console.log("Modulus = ",mod);
+
+
+let a = 4;
+let b = 3;
+
+let power = a ** b;
+console.log("Exponent = ",power);
+
+
