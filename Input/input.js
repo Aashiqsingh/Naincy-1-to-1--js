ca0b601979ba270fc38df887d0d5adaef58dd451
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-10-19 10:22:23 +0530

    input

---
+++ Input/input.js
@@ -0,0 +1,11 @@
+// alert("hello naincy kamnani...")
+
+// var name = prompt("Enter your name");
+// console.log(name);
+
+var a = parseInt(prompt("Enter first number :"));
+var b = parseInt(prompt("Enter second number :"));
+
+var add = a + b;
+console.log("Addition = ",add);
+
