34c6ff4e6f9f44baf18d63d477335864c119dff0
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-10-19 10:06:57 +0530

    marks

---
+++ Operator/Marks.js
@@ -0,0 +1,12 @@
+var eng = 81;
+var math = 87;
+var comp = 92;
+var sci = 93;
+
+
+var total = eng + math + comp + sci;
+console.log("Total marks of all subject = ",total);
+
+var per = total / 4;
+console.log("Percentage of all subject = ",per);
+
