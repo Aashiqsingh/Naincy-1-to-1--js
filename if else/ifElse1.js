e8806eca81452e87a57a47e523ac6256de839063
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-10-22 12:37:08 +0530

    if else

---
+++ if else/ifElse1.js
@@ -0,0 +1,33 @@
+// var a = 32;
+// var b = 23;
+
+
+// if(a > b){
+//     console.log(a +" is greater");
+    
+// }
+// else{
+//     console.log(b +" is greater");
+// }
+
+
+// var num = 44;
+
+// if(num % 2 == 0){
+//     console.log(num +" is an even number");
+// }
+// else{
+//     console.log(num +" is an odd number");
+// }
+
+
+
+var num = -4;
+
+if(num > 0){
+    console.log(num + " is Positive");
+    
+}
+else{
+    console.log(num + " is Negative");
+}
