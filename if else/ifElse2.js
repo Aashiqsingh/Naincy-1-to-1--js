1497d9906c1110547748d4564e5ea49e084c1a66
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-11-09 10:15:41 +0530

    gretter than

---
+++ if else/ifElse2.js
@@ -0,0 +1,45 @@
+// var a = 18;
+// var b = 15;
+// var c = 14;
+
+
+// if(a>b){
+//     if(a>c){
+//         console.log("A is greater");
+        
+//     }
+//     else{
+//         console.log("C is greater");
+//     }
+// }
+// else{
+//     if(b>c){
+//         console.log("B is greater");
+        
+//     }
+//     else{
+//         console.log("C is greater");
+//     }
+// }
+
+
+
+var isActive = false;
+var age = 17;
+
+if(isActive == true)
+{
+    console.log("Active")
+    
+    if(age > 18){
+        console.log("You are eligible for vote");
+        
+    }
+    else{
+        console.log("You are not eligible for vote");
+    }
+}
+else{
+    console.log("DEAD..");
+    
+}
