70ad212874d769ba3fcd05dacf0478dde9727263
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-11-12 12:51:59 +0530

    factorial

--- ForLoop/forLoop1.js
+++ ForLoop/forLoop1.js
@@ -7,11 +7,20 @@
 
 // odd number between 0 to 100
 
-for(let i=1;i<=100;i++){
+// for(let i=1;i<=100;i++){
     
-    if(i % 2 == 1){
-        console.log(i);
+//     if(i % 2 == 1){
+//         console.log(i);
         
-    }
+//     }
         
+// }
+
+let fact = 1;
+
+for(let i=1;i<=5;i++){
+
+    fact = fact * i;
 }
+
+console.log("Factorial = ",fact);
