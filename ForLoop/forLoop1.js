e2a585d4e24fe5c9de00034cc49a699bbd0a01e5
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-11-12 12:15:56 +0530

    forloop

---
+++ ForLoop/forLoop1.js
@@ -0,0 +1,17 @@
+
+
+// for(let i=1;i<=100;i++){
+//     console.log(i);
+    
+// }
+
+// odd number between 0 to 100
+
+for(let i=1;i<=100;i++){
+    
+    if(i % 2 == 1){
+        console.log(i);
+        
+    }
+        
+}
