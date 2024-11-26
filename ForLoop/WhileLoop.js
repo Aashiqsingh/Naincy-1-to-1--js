b3eef8bc43ccc3f8dd6bc6f44900c7cd71175245
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-11-26 12:20:36 +0530

    do while loop

--- ForLoop/WhileLoop.js
+++ ForLoop/WhileLoop.js
@@ -31,12 +31,12 @@
 // }
 
 
-let i=1;
-while(i<=50)
-{
-    if(i%2==0)
-    {
-        console.log(i);
+// let i=1;
+// while(i<=50)
+// {
+//     if(i%2==0)
+//     {
+//         console.log(i);
         
-    }
-}
+//     }
+// }
---
+++ ForLoop/doWhileLoop.js
@@ -0,0 +1,35 @@
+// for(i=0;i<5;i++){
+
+// }
+
+
+// for(initialization; condition ; increment/decrement){
+
+//     // code executuin
+// }
+
+
+// i = 0;  // initialization
+
+// while(i < 5){ // condition
+
+//     // code execution
+//     i++;  // increment/decrement
+// }
+
+
+// let i=1;
+
+// do{
+//     console.log(i);
+//     i++;
+// }while(i <= 5);
+
+let num = 7;
+
+let i = 1;
+
+do{
+    console.log(num + "*" + i + "=" + num * i);
+    i++;
+}while(i <= 10);
