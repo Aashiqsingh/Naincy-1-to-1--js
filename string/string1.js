08afc97b9945e4ca55915c29c494d4cce14c6ec2
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-11-30 10:18:07 +0530

    string

---
+++ string/string1.js
@@ -0,0 +1,29 @@
+var str = "Royal technosoft pvt ltd ";
+
+console.log(str);
+// console.log(str.length);
+
+// // console.log(str.replaceAll("soft","hard"));
+// console.log(str.replaceAll("o","z"));
+
+// console.log(str.slice(6,16));
+
+
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+
+// console.log(str.endsWith(" "));
+// console.log(str.startsWith("o"));
+
+// console.log(str.repeat(4));
+
+// console.log(str.split(""));
+
+
+
+
+
+
+
