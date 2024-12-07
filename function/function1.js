ec5522868c4a14fed8433cb7453406ca2bf6fd20
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-12-07 10:19:09 +0530

    without argument without return

---
+++ function/function1.js
@@ -0,0 +1,32 @@
+// function -- code reusiblity 
+
+// 1. without argument without return 
+// 2. without argument with return 
+// 3. with argument without return 
+// 4. with argument with return 
+
+// without argument without return
+
+function demo()
+{
+    console.log("Hello naincy Good morning...");
+    
+}
+
+// demo()
+// demo()
+
+// function isOdd()
+// {
+//     let num = 14;
+//     if(num % 2 == 1)
+//     {
+//         console.log("Number is Odd ",num);
+        
+//     }
+//     else{
+//         console.log("Number is Even ",num);
+//     }
+// }
+
+// isOdd()
