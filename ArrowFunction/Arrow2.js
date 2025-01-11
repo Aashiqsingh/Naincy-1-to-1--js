389786621d20eeb0f3f1dac544ddc4e154554819
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-01-11 10:54:12 +0530

    arrow

---
+++ ArrowFunction/Arrow2.js
@@ -0,0 +1,12 @@
+const EvneOdd = (x)=>{
+    if(x % 2 == 0)
+    {
+        console.log(x + " is Even number.");
+        
+    }
+    else{
+        console.log(x + " is Odd number.");
+    }
+}
+
+EvneOdd(11)
