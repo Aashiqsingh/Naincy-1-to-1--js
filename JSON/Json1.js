0473b7c647ac09c54e5fffab83ec9c6988688da1
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-02-08 10:27:12 +0530

    json

---
+++ JSON/Json1.js
@@ -0,0 +1,22 @@
+//  JSON --> JavaScript object notation
+
+var student = {
+    roll:101,
+    name:"naincy",
+    age:19,
+    gender:"female",
+    mobile:9717259377,
+    email:"naincy@gmail.com",
+    isActive:true,
+
+}
+
+console.log(student);
+console.log(student.roll);
+console.log(student.name);
+console.log(student.age);
+console.log(student.gender);
+console.log(student.mobile);
+console.log(student.email);
+console.log(student.isActive);
+
