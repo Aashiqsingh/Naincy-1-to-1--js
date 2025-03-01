83353df01523e098624c4a45326fe9ea1ea111c5
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-01 10:16:47 +0530

    array

---
+++ Array/Array1.js
@@ -0,0 +1,39 @@
+var arr = ["naincy","tarun","varun","harsh","kashyap","parul","basant"];
+
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+
+
+// for(let i=0;i<arr.length;i++)
+// {
+//     console.log(arr[i]);
+    
+// }
+
+// console.log(arr);
+
+
+// arr.push("aashiq","harshita","aarti") // add element from last
+
+// console.log(arr);
+
+
+// arr.pop();  // remove from last index
+
+
+// console.log(arr);
+
+
+// arr.unshift("aashiq","harshita","aarti");
+// console.log(arr);
+
+// arr.shift();
+// console.log(arr);
+
+
+
+
+
+
