c16579a68a3570f50fd6c0dba52b9c2bb0fdc1c9
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-08 10:27:35 +0530

    onmouseenter

---
+++ Event/Event2.js
@@ -0,0 +1,11 @@
+function changeSize(){
+
+    var mydiv = document.getElementById('mydiv');
+
+    mydiv.style.height = "400px";
+    mydiv.style.width = "400px";
+    mydiv.style.backgroundColor = "green";
+    mydiv.style.borderRadius = "50%";
+    mydiv.style.backgroundImage = "url(https://img.etimg.com/thumb/width-420,height-315,imgsize-2014934,resizemode-75,msid-113998626/news/india/23-years-of-narendra-modi-in-public-office-what-has-been-achieved-and-whats-still-to-come/23-years-of-narendra-modi-in-public-office.jpg)"
+    mydiv.style.backgroundSize = "cover";
+}
