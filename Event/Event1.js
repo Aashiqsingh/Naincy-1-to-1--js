b91b5aa39b60cec3ab6f99e01e302d7605b03aa2
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-08 10:12:29 +0530

    onclick

---
+++ Event/Event1.js
@@ -0,0 +1,9 @@
+function changeTxt(){
+    var txt = document.getElementById('txt');
+
+    txt.innerHTML = "Naincy Kamnani";
+    txt.style.backgroundColor = "red";
+    txt.style.color = "white";
+    txt.style.height = "200px";
+    txt.style.width = "200px";
+}
