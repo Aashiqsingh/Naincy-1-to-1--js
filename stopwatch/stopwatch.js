674ad6560c6373b10cd0c2b86a44d6c93488cff4
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-05-31 10:19:46 +0530

    stopwatch

---
+++ stopwatch/stopwatch.js
@@ -0,0 +1,31 @@
+var hour = document.getElementById("hour");
+var min = document.getElementById("min");
+var sec = document.getElementById("sec");
+
+var hours = 0;
+var mins = 0;
+var secs = 0;
+
+function start2(){
+
+    secs++;
+    if(secs == 60){
+        secs = 0;
+        mins++;
+    }
+    else if(mins == 60){
+        mins = 0;
+        hours++;
+    }
+
+    hour.innerHTML = hours < 10 ? "0"+hours : hours;
+    min.innerHTML = mins < 10 ? "0"+mins : mins;
+    sec.innerHTML = secs < 10 ? "0"+secs : secs;
+}
+
+
+function start(){
+    x = setInterval(()=>{
+        start2()
+    },1000)
+}
